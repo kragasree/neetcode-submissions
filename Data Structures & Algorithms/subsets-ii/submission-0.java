ec26718d41cd610214e7df7427395a5b026a098b
Ragasree <102582161+kragasree@users.noreply.github.com>
2026-06-23 00:02:59 +0530

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        // to sort the array so that we can easily find the dup numbers
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        find(0, nums, new ArrayList<>(), ans);
+        return ans;
+    }
+
+    public void find(int start, int nums[], List<Integer> res, List<List<Integer>> ans) {
+        ans.add(new ArrayList<>(res));
+
+        for(int i=start; i<nums.length; i++) {
+            //to skip the same element
+            if(i!=start && nums[i] == nums[i-1]) {
+                continue;
+            }
+            //add the ele to list
+            res.add(nums[i]);
+            //find next subsets
+            find(i+1,nums,res,ans);
+            res.remove(res.size() - 1);
+        }
+
+    }
+}
