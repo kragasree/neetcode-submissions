b2d7a2ef545a517d25988e916e87d6c2394d2606
Ragasree <102582161+kragasree@users.noreply.github.com>
2026-06-28 20:35:27 +0530

    Add: non-cyclical-number - submission-1

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> st = new HashSet<>();
+
+        // if number already seen, exit
+        while(!st.contains(n)) {
+            st.add(n);
+            int ans = 0;
+            // find square of that number
+            while(n>0) {
+                int r = n%10;
+                r = r * r;
+                ans = ans + r;
+                n = n/10;
+            }
+            n = ans;
+            if(n == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //TC:O(n^2)
+}
