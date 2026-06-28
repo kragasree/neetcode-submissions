47d21d61b7f9829b8dfedddc0e33c3815390e62f
Ragasree <102582161+kragasree@users.noreply.github.com>
2026-06-28 19:14:29 +0530

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        Arrays.sort(stones);
+        List<Integer> al = new ArrayList<>();
+        for(int i=0; i<stones.length; i++) {
+            al.add(stones[i]);
+        }
+
+        while(al.size() > 1) {
+            int n = al.size();
+            int x = al.get(n-1);
+            int y = al.get(n-2);
+            int newEle = 0;
+            if(x < y){
+                newEle = y-x;
+            } else {
+                newEle = x-y;
+            }
+            al.remove(n-1);  
+            al.remove(n-2); 
+            al.add(newEle);
+            // bcoz to destroy heavy stones first
+            Collections.sort(al);
+        }
+
+        return al.isEmpty() ? 0 : al.get(0);
+    }
+}
