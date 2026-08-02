39052f7fcabb8af5d683da3435a35859d1ca1936
Ragasree <102582161+kragasree@users.noreply.github.com>
2026-08-02 19:00:42 +0530

    Add: pow-x-n - submission-1

---
+++ Data Structures & Algorithms/pow-x-n/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(x == 0) {
+            return 0;
+        }
+        if(n == 0) {
+            return 1;
+        }
+
+        double ans = 1;
+        for(int i=0; i<Math.abs(n); i++) {
+            ans = ans * x;
+        }
+
+        if(n >= 0) {
+            return ans;
+        } else {
+            return 1/ans;
+        }
+    }
+}
